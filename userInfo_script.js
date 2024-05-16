1e9e4607648367b4fb43b1c60ee90d166793420e
Eth-Ski <105698499+Eth-Ski@users.noreply.github.com>
2024-05-16 13:28:45 +1000

    Update userInfo_script.js

--- userInfo_script.js
+++ userInfo_script.js
@@ -1,9 +1,17 @@
 document.getElementById('detailsUpdate').addEventListener('submit', function(event)
-{
+{	
     event.preventDefault();
+	savedUser = localStorage.getItem('user');
+	userOne = JSON.parse(savedUser);
     const username=document.getElementById('name').value;
     document.getElementById('username').textContent=username.split(' ')[0];
-    alert('Details updated!')
+	userOne.name = document.getElementById('name').value;
+	userOne.email = document.getElementById('email').value;
+	userOne.phoneNo = document.getElementById('phone').value;
+	userOne.Address = document.getElementById('address').value;
+	localStorage.setItem('user', JSON.stringify(userOne));
+	savedUser = localStorage.getItem('user');
+    displayDetails();
 });
 
 function addFavorite()
@@ -11,10 +19,25 @@ function addFavorite()
     const newFavorite=document.getElementById('newFavorite').value;
     if(newFavorite)
         {
-            const list = document.getElementById('favoriteList');
+            const list = document.getElementById('favoritesList');
             const entry = document.createElement('li');
             entry.textContent=newFavorite;
             list.appendChild(entry);
             document.getElementById('newFavorite').value='';
         }
+}
+
+function updateDetails()
+{	
+	const savedUser = localStorage.getItem('user');
+	userOne = JSON.parse(savedUser);
+	document.getElementById('detailsUpdate').innerHTML =
+	'<form id="detailsUpdate">'
+		+'Username: <input type="text" id="name" value="'+userOne.name+'" required>'
+		+'Email: <input type="email" id="email" value="'+userOne.email+'" required>'
+		+'Phone Number:<input type="text" id="phone" value="'+userOne.phoneNo+'" required>'
+		+'Address: <input type="text" id="address" value="'+userOne.address+'" required>'
+		+'<button type="submit">Confirm</button>&nbsp;&nbsp;'
+		+'<button onClick="displayDetails()">Cancel</button>'
+		+'</form>'
 }
