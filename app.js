e0d9eca47a4fad2e7842bb9062fc05b728cb08e9
Eth-Ski <105698499+Eth-Ski@users.noreply.github.com>
2024-05-17 00:51:45 +1000

    Update app.js

--- app.js
+++ app.js
@@ -50,21 +50,45 @@ $(".next-menus").on("click", function(e) {
 
 document.addEventListener('DOMContentLoaded', function() {
     const modals = {
-        shrimpSoupModal: document.getElementById("ShrimpSoupModal"),
-        pizzaModal: document.getElementById("PizzaModal"),
-        doubleCheeseBurgerModal: document.getElementById("DoubleCheeseBurgerModal"),
-        wineModal: document.getElementById("WineModal"),
-        hotCafeLatteModal: document.getElementById("HotCafeLatteModal"),
-        freshSaladModal: document.getElementById("FreshSaladModal")
+        signatureLasagnaModal: document.getElementById("SignatureLasagnaModal"),
+        capreseSaladModal: document.getElementById("CapreseSaladModal"),
+        classicTiramisuModal: document.getElementById("ClassicTiramisuModal"),
+        boeufBourguignonModal: document.getElementById("BoeufBourguignonModal"),
+        ratatouilleModal: document.getElementById("RatatouilleModal"),
+        tarteTatinModal: document.getElementById("TarteTatinModal"),
+        butterChickenModal: document.getElementById("ButterChickenModal"),
+        palakPaneerModal: document.getElementById("PalakPaneerModal"),
+        mangoLassiModal: document.getElementById("MangoLassiModal"),
+        sushiPlatterModal: document.getElementById("SushiPlatterModal"),
+        ramenBowlModal: document.getElementById("RamenBowlModal"),
+        tempuraBentoModal: document.getElementById("TempuraBentoModal"),
+        spicyChickenModal: document.getElementById("SpicyChickenModal"),
+        mapoTofuModal: document.getElementById("MapoTofuModal"),
+        beijingDuckModal: document.getElementById("BeijingDuckModal"),
+        tacosModal: document.getElementById("TacosModal"),
+        mexicanBurritoModal: document.getElementById("MexicanBurritoModal"),
+        molePoblanoModal: document.getElementById("MolePoblanoModal"),
     };
 
     const buttons = {
-        shrimpSoupBtn: document.querySelector(".detail-card img[src='images/seafood.jpeg']"),
-        pizzaBtn: document.querySelector(".detail-card img[src='images/pizza.jpeg']"),
-        doubleCheeseBurgerBtn: document.querySelector(".detail-card img[src='images/burger.jpeg']"),
-        wineBtn: document.querySelector(".detail-card img[src='images/wine.jpeg']"),
-        hotCafeLatteBtn: document.querySelector(".detail-card img[src='images/coffee.jpeg']"),
-        freshSaladBtn: document.querySelector(".detail-card img[src='images/salad.jpeg']")
+        signatureLasagnaBtn: document.querySelector(".detail-card img[src='https://images.unsplash.com/photo-1551443874-e8d6a8e561aa?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80']"),
+        capreseSaladBtn: document.querySelector(".detail-card img[src='https://images.unsplash.com/photo-1541592106381-b31e9677c0e5?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80']"),
+        classicTiramisuBtn: document.querySelector(".detail-card img[src='https://images.unsplash.com/photo-1543353071-873f17a7a088?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80']"),
+        boeufBourguignonBtn: document.querySelector(".detail-card img[src='https://food-images.files.bbci.co.uk/food/recipes/boeuf_bourguignon_25475_16x9.jpg']"),
+        ratatouilleBtn: document.querySelector(".detail-card img[src='https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg']"),
+        tarteTatinBtn: document.querySelector(".detail-card img[src='https://images.immediate.co.uk/production/volatile/sites/30/2020/08/tarte-tatin-502b8b9.jpg']"),
+        butterChickenBtn: document.querySelector(".detail-card img[src='https://littlespicejar.com/wp-content/uploads/2016/02/Finger-Lickin-Butter-Chicken-14-735x1103.jpg']"),
+        palakPaneerBtn: document.querySelector(".detail-card img[src='https://www.vegrecipesofindia.com/wp-content/uploads/2021/06/palak-paneer-3.jpg']"),
+        mangoLassiBtn: document.querySelector(".detail-card img[src='https://www.cookwithmanali.com/wp-content/uploads/2017/05/Best-Mango-Mango-Lassi-Recipe.jpg']"),
+        sushiPlatterBtn: document.querySelector(".detail-card img[src='https://www.seafoodandmore.com.au/cdn/shop/products/ADF0AD79-7824-4C6C-94C0-6BE86826663B_23255145-1fae-4c8a-8efe-bf722b43a857.jpg']"),
+        ramenBowlBtn: document.querySelector(".detail-card img[src='https://www.marionskitchen.com/wp-content/uploads/2024/04/20240329_MIW_15-Minute-Creamy-Korean-Ramen_Bangkok-7-scaled.webp']"),
+        tempuraBentoBtn: document.querySelector(".detail-card img[src='https://media-cdn.tripadvisor.com/media/photo-s/12/5e/83/85/bento-box-a-2-prawn-tempura.jpg']"),
+        spicyChickenBtn: document.querySelector(".detail-card img[src='https://christieathome.com/wp-content/uploads/2022/02/General-Tso-Chicken-8.jpg']"),
+        mapoTofuBtn: document.querySelector(".detail-card img[src='https://hips.hearstapps.com/hmg-prod/images/mapo-tofu-2-jpg-1642115176.jpg']"),
+        beijingDuckBtn: document.querySelector(".detail-card img[src='https://anisland.cooking/wp-content/uploads/2022/02/Roast-Cantonese-Duck-2022.jpg']"),
+        tacosBtn: document.querySelector(".detail-card img[src='https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/29/0/0149359_Making-Taco_s4x3.jpg.rend.hgtvcom.1280.960.suffix/1371603491866.jpeg']"),
+        mexicanBurritoBtn: document.querySelector(".detail-card img[src='https://www.seriouseats.com/thmb/lOdEqFZsV3LOzX5Pc2Y6XCJuvTs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2020__10__20201002-mission-style-burrito-jillian-atkinson-2-6841455590ed4c3981529b232166643a.jpg']"),
+        molePoblanoBtn: document.querySelector(".detail-card img[src='https://www.mccormick.com/-/media/project/oneweb/mccormick-us/mccormick/recipe-categories/m/1376x774/mole_poblano_chicken_1376x774.jpg?rev=be4f11cdcbe741c29f1635f48d7c5506&vd=20210922T165012Z&extension=webp&hash=A1E6549E6559FC1F4DBA8FD923D07893']"),
     };
 
     const spans = document.getElementsByClassName("close");
@@ -104,12 +128,25 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 const productImages = {
-    'Shrimp Soup': 'images/seafood.jpeg',
-    'Pizza': 'images/pizza.jpeg',
-    'Wine': 'images/wine.jpeg',
-    'Double Cheese Burger': 'images/burger.jpeg',
-    'Hot Cafe Latte': 'images/coffee.jpeg',
-    'Fresh Salad': 'images/salad.jpeg'
+    'Signature Lasagna': "https://images.unsplash.com/photo-1551443874-e8d6a8e561aa?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+    'Caprese Salad': "https://images.unsplash.com/photo-1541592106381-b31e9677c0e5?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+    'Classic Tiramisu': "https://images.unsplash.com/photo-1543353071-873f17a7a088?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+    'Boeuf Bourguignon': "https://food-images.files.bbci.co.uk/food/recipes/boeuf_bourguignon_25475_16x9.jpg",
+    'Ratatouille': "https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg",
+    'Tarte Tatin': "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/tarte-tatin-502b8b9.jpg",
+    'Butter Chicken': "https://littlespicejar.com/wp-content/uploads/2016/02/Finger-Lickin-Butter-Chicken-14-735x1103.jpg",
+    'Palak Paneer': "https://www.vegrecipesofindia.com/wp-content/uploads/2021/06/palak-paneer-3.jpg",
+    'Mango Lassi': "https://www.cookwithmanali.com/wp-content/uploads/2017/05/Best-Mango-Mango-Lassi-Recipe.jpg",
+    'Sushi Platter': "https://www.seafoodandmore.com.au/cdn/shop/products/ADF0AD79-7824-4C6C-94C0-6BE86826663B_23255145-1fae-4c8a-8efe-bf722b43a857.jpg",
+    'Ramen Bowl': "https://www.marionskitchen.com/wp-content/uploads/2024/04/20240329_MIW_15-Minute-Creamy-Korean-Ramen_Bangkok-7-scaled.webp",
+    'Tempura Bento': "https://media-cdn.tripadvisor.com/media/photo-s/12/5e/83/85/bento-box-a-2-prawn-tempura.jpg",
+    'Spicy Chicken': "https://christieathome.com/wp-content/uploads/2022/02/General-Tso-Chicken-8.jpg",
+    'Mapo Tofu': "https://hips.hearstapps.com/hmg-prod/images/mapo-tofu-2-jpg-1642115176.jpg",
+    'Beijing Duck': "https://anisland.cooking/wp-content/uploads/2022/02/Roast-Cantonese-Duck-2022.jpg",
+    'Tacos': "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/29/0/0149359_Making-Taco_s4x3.jpg.rend.hgtvcom.1280.960.suffix/1371603491866.jpeg",
+    'Mexican Burrito': "https://www.seriouseats.com/thmb/lOdEqFZsV3LOzX5Pc2Y6XCJuvTs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2020__10__20201002-mission-style-burrito-jillian-atkinson-2-6841455590ed4c3981529b232166643a.jpg",
+    'molePoblano': "https://www.mccormick.com/-/media/project/oneweb/mccormick-us/mccormick/recipe-categories/m/1376x774/mole_poblano_chicken_1376x774.jpg?rev=be4f11cdcbe741c29f1635f48d7c5506&vd=20210922T165012Z&extension=webp&hash=A1E6549E6559FC1F4DBA8FD923D07893",
+
 };
 
 let cart = [];
@@ -388,4 +425,41 @@ function updateTotal(i, amount){
     localStorage.setItem('cart', JSON.stringify(uCart));
     uCart = JSON.parse(localStorage.getItem('cart'));
     updateSubTotal();
+}
+
+function hideAll(type){
+	for (const s of document.getElementsByClassName('detail-card')){
+		s.style.display = "none";
+	}
+	if(type == "western"){
+		document.getElementById("bites").style.display = "block";
+		document.getElementById("steakhouse").style.display = "block";
+	}
+	else if(type == "italian"){
+		document.getElementById("italiano").style.display = "block";
+	}
+	else if(type == "mexican"){
+		document.getElementById("mexicano").style.display = "block";
+	}
+	else if(type == "icecream"){
+		document.getElementById("scoops").style.display = "block";
+	}
+	else if(type == "cafe"){
+		document.getElementById("bistro").style.display = "block";
+	}
+	else if(type == "asian"){
+		document.getElementById("nami").style.display = "block";
+		document.getElementById("thai").style.display = "block";
+		document.getElementById("dragon").style.display = "block";
+	}
+	else if(type == "indian"){
+		document.getElementById("symphony").style.display = "block";
+	}
+	
+}
+
+function showAll(){
+	for (const s of document.getElementsByClassName('detail-card')){
+		s.style.display = "block";
+	}
 }
